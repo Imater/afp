af53530512d5637ed37959864063825243ef531b
Imater <eugene.leonar@gmail.com>
2016-01-25 20:34:18 +0500

    turn off faq

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -272,7 +272,7 @@ class About extends Component {
 
             }
             {
-              language === 'ru' ? (
+              language === 'ru_off' ? (
                 <Link className="aboutCategory-item aboutCategory-item--3" to="/faq">
                   <img className="aboutCategory-img" src="/assets/img/about/icon-messages.svg" width="73"/>
                   <span className="aboutCategory-label">{'FAQ'}</span>
